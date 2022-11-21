75fda82a17bca21a6d2b8722808c0a0491d07770
Vladimir Sidorenko <vladmir.sidorenko@gmail.com>
2022-11-21 15:00:29 +0300

    Release 1.0

---
+++ src/main/java/org/example/IntsCalculator.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class IntsCalculator implements Ints {
+
+    @Override
+    public int sum(int arg0, int arg1) {
+        return 0;
+    }
+
+    @Override
+    public int mult(int arg0, int arg1) {
+        return 0;
+    }
+
+    @Override
+    public int pow(int a, int b) {
+        return 0;
+    }
+}
